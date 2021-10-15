f599888dab9b5ca83229b24bbc3305b279c812be
Mike <mstileswork35@outlook.com>
2021-10-15 22:54:29 +0500

    Added tailwind css, firebase auth and different components & views

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,27 +1,58 @@
 <template>
-  <header>
-    <nav class="container">
-      <div class="branding">
-        <router-link class="header" :to="{ name: 'Home' }"
+  <header class="bg-white px-6 shadow-md z-50">
+    <nav class="container flex py-6">
+      <div class="flex items-center">
+        <router-link
+          class="font-semibold text-2xl text-black no-underline"
+          :to="{ name: 'Home' }"
           >FireBlogs</router-link
         >
       </div>
-      <div class="nav-links">
-        <ul>
-          <router-link class="link" to="#">Home</router-link>
-          <router-link class="link" to="#">Blogs</router-link>
-          <router-link class="link" to="#">Create Post</router-link>
-          <router-link class="link" to="#">Login/Register</router-link>
+      <div class="relative flex items-center justify-end flex-1">
+        <ul class="mr-8 last:mr-0" v-show="!mobile">
+          <router-link
+            class="font-medium px-2 transition hover:text-blue-400"
+            :to="{ name: 'Home' }"
+            >Home</router-link
+          >
+          <router-link
+            class="font-medium px-2 transition hover:text-blue-400"
+            :to="{ name: 'Blogs' }"
+            >Blogs</router-link
+          >
+          <router-link
+            class="font-medium px-2 transition hover:text-blue-400"
+            to="#"
+            >Create Post</router-link
+          >
+          <router-link
+            class="font-medium px-2 transition hover:text-blue-400"
+            :to="{ name: 'Login' }"
+            >Login/Register</router-link
+          >
         </ul>
       </div>
     </nav>
-    <menuIcon />
+    <menuIcon
+      @click="toggleMobileNav"
+      class="cursor-pointer absolute top-8 right-6 h-6 w-auto"
+      v-show="mobile"
+    />
     <transition name="mobile-nav">
-      <ul>
-        <router-link class="link" to="#">Home</router-link>
-        <router-link class="link" to="#">Blogs</router-link>
-        <router-link class="link" to="#">Create Post</router-link>
-        <router-link class="link" to="#">Login/Register</router-link>
+      <ul
+        v-show="mobileNav"
+        class="p-5 w-3/6 max-w-xs flex flex-col fixed h-full bg-gray-800 top-0 left-0"
+      >
+        <router-link class="py-1 text-white" :to="{ name: 'Home' }"
+          >Home</router-link
+        >
+        <router-link class="py-1 text-white" :to="{ name: 'Blogs' }"
+          >Blogs</router-link
+        >
+        <router-link class="py-1 text-white" to="#">Create Post</router-link>
+        <router-link class="py-1 text-white" :to="{ name: 'Login' }"
+          >Login/Register</router-link
+        >
       </ul>
     </transition>
   </header>
@@ -31,8 +62,48 @@
 import menuIcon from "../assets/Icons/bars-regular.svg";
 export default {
   name: "navigation",
-  components: menuIcon,
+  components: { menuIcon },
+  data() {
+    return {
+      mobile: null,
+      mobileNav: null,
+      windowWidth: null,
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.checkScreen);
+    this.checkScreen();
+  },
+  methods: {
+    checkScreen() {
+      this.windowWidth = window.innerWidth;
+      if (this.windowWidth <= 750) {
+        this.mobile = true;
+        return;
+      }
+      this.mobile = false;
+      this.mobileNav = false;
+    },
+
+    toggleMobileNav() {
+      this.mobileNav = !this.mobileNav;
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.mobile-nav-enter-active,
+.mobile-nav-leave-active {
+  transition: all 1s ease;
+}
+.mobile-nav-enter {
+  transform: translateX(-250px);
+}
+.mobile-nav-enter-to {
+  transform: translateX(0);
+}
+.mobile-nav-leave-to {
+  transform: translateX(-250px);
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,76 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase/app";
+import "firebase/auth";
+import db from "../firebase/firebaseInit";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    sampleBlogCards: [
+      {
+        blogTitle: "Blog Card #1",
+        blogCoverPhoto: "stock-1",
+        blogData: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #2",
+        blogCoverPhoto: "stock-2",
+        blogData: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #3",
+        blogCoverPhoto: "stock-3",
+        blogData: "May 1, 2021",
+      },
+      {
+        blogTitle: "Blog Card #4",
+        blogCoverPhoto: "stock-4",
+        blogData: "May 1, 2021",
+      },
+    ],
+    editPost: null,
+    user: null,
+    profileAdmin: null,
+    profileEmail: null,
+    profileFirstName: null,
+    profileLastName: null,
+    profileUsername: null,
+    profileId: null,
+    profileInitials: null,
   },
   mutations: {
+    toggleEditPost(state, payload) {
+      state.editPost = payload;
+    },
+    updateUser(state, payload) {
+      state.user = payload;
+    },
+    setProfileInfo(state, doc) {
+      state.profileId = doc.id;
+      state.profileEmail = doc.data().email;
+      state.profileFirstName = doc.data().firstName;
+      state.profileLastName = doc.data().lastName;
+      state.profileUsername = doc.data().username;
+      console.log(state.profileEmail);
+    },
+    setProfileInitials(state) {
+      state.profileInitials =
+        state.profileFirstName.match(/(\b\S)?/g).join("") +
+        state.profileLastName.match(/(\b\S)?/g).join("");
+    },
   },
   actions: {
+    async getCurrentUser({ commit }) {
+      const dataBase = await db
+        .collection("users")
+        .doc(firebase.auth().currentUser.uid);
+      const dbResults = await dataBase.get();
+      commit("setProfileInfo", dbResults);
+      commit("setProfileInitials");
+      console.log(dbResults);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
